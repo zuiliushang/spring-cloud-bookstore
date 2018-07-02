2257cf9da7e76bf419217b354503abf0f982d2d9
xusihan <383688501@qq.com>
2018-07-02 09:30:44 +0800

    config

--- hr-guqing-cloud-bookstore/src/main/java/hr/guqing/cloud/bookstore/GuQingBookStoreApp.java
+++ hr-guqing-cloud-bookstore/src/main/java/hr/guqing/cloud/bookstore/GuQingBookStoreApp.java
@@ -1,18 +1,16 @@
-	package hr.guqing.cloud.config;
+package hr.guqing.cloud.bookstore;
 
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
-@EnableConfigServer
 @EnableDiscoveryClient
-public class GuQingConfigApp {
-	
-	public static void main(String[] args) {
-		new SpringApplicationBuilder(GuQingConfigApp.class).run(args);
+public class GuQingBookStoreApp {
 
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(GuQingBookStoreApp.class).web(WebApplicationType.REACTIVE).run(args);
 	}
 	
 }
