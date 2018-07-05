fcfb8ebf8ac44eef32007ff94d36e9b7a1ca3361
xusihan <383688501@qq.com>
2018-07-05 15:13:00 +0800

    config

--- hr-guqing-cloud-bookstore/src/main/java/hr/guqing/cloud/bookstore/GuQingBookStoreApp.java
+++ hr-guqing-cloud-bookstore/src/main/java/hr/guqing/cloud/bookstore/GuQingBookStoreApp.java
@@ -7,26 +7,35 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.server.RequestPredicates;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
+import reactor.core.publisher.Mono;
+
 @SpringBootApplication
 @EnableDiscoveryClient
+@RestController
 public class GuQingBookStoreApp {
 
 	@Value("${greeting}")
 	String greet;
 	
-	@Bean
+	/*@Bean
 	public RouterFunction<?> routerFunction(){
 		return RouterFunctions.route(RequestPredicates.GET("/greet"), 
 				(r)->(ServerResponse
 						.ok()
 						.contentType(MediaType.APPLICATION_JSON)
 						.body(BodyInserters.fromObject(greet))));
+	}*/
+	@GetMapping("/greet")
+	public Mono<String> greeta(){
+		return Mono.just(greet);
 	}
 	
 	
--- hr-guqing-cloud-gateway/src/main/java/hr/guqing/cloud/gateway/GuQingGateWayApp.java
+++ hr-guqing-cloud-gateway/src/main/java/hr/guqing/cloud/gateway/GuQingGateWayApp.java
@@ -2,38 +2,47 @@ package hr.guqing.cloud.gateway;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.actuate.trace.http.HttpTrace.Response;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.server.RequestPredicates;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
+import reactor.core.publisher.Mono;
+
 @SpringBootApplication
 @EnableZuulProxy
+@RestController
 public class GuQingGateWayApp {
 	
 	@Value("${greeting}")
 	String greet;
 	
-	@Bean
+	@GetMapping(value="forward")
+	public Mono<String> greeting(){
+		return Mono.just(greet);
+	}
+	
+	/*@Bean
 	public RouterFunction<?> routerFunction(){
-		return RouterFunctions.route(RequestPredicates.GET("/greet"), 
+		return RouterFunctions.route(RequestPredicates.GET("/forward"), 
 				(r)->(ServerResponse
 						.ok()
 						.contentType(MediaType.APPLICATION_JSON)
 						.body(BodyInserters.fromObject(greet))));
 	}
-	
+	*/
 	
 	public static void main(String[] args) {
-		new SpringApplicationBuilder(GuQingGateWayApp.class).web(WebApplicationType.REACTIVE).run(args);
+		new SpringApplicationBuilder(GuQingGateWayApp.class).run(args);
 	}
 	
 }
